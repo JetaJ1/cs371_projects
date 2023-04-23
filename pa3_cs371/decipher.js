f25e23be39f4e7d6fdc1dd7387af3fdb880b61cd
ssorpg <42558066+ssorpg@users.noreply.github.com>
2023-04-22 21:00:24 -0400

    Added documentation

--- pa3_cs371/decipher.js
+++ pa3_cs371/decipher.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
-fs.readFile("./PA3support/Cryptanalysis/ciphertext.txt", (error, output) => crackCipher(error, output));
+fs.readFile("./PA3support/Cryptanalysis/ciphertext.txt", (error, output) => decipher(error, output));
 
 // Source that might be helpful - displays English letter frequencies
 // https://www3.nd.edu/~busiforc/handouts/cryptography/letterfrequencies.html
-const alphabet = [
+const alphabetMapping = [
 	{ letter: "a", replacement: "c" },
 	{ letter: "b", replacement: "i" },
 	{ letter: "c", replacement: "o" },
@@ -33,45 +33,19 @@ const alphabet = [
 	{ letter: "z", replacement: "x" }
 ];
 
-function crackCipher(error, output) {
+function decipher(error, output) {
 	const outputAsStr = output.toString().toLowerCase();
 	let decipheredStr = outputAsStr;
 
-	let numCharacters = 0;
-	const occurances = [];
-	const proportions = [];
-
-	for (let i = 0; i < alphabet.length; i++) {
-		const letter = alphabet[i];
-		occurances.push(getOccurances(outputAsStr, letter.letter));
-		numCharacters += occurances[i];
-	}
-
-	for (let i = 0; i < occurances.length; i++) {
-		const occurance = occurances[i];
-		proportions.push(occurance / numCharacters);
-	}
-
-	// console.log(numCharacters);
-	// console.log(occurances);
-
-	for (let i = 0; i < proportions.length; i++) {
-		const letter = alphabet[i];
-		const proportion = proportions[i];
-		console.log(`The letter ${ letter.letter } appears ${ (proportion * 100).toFixed(2) }% of the time`);
-	}
-
-	console.log("");
-
-	for (let i = 0; i < alphabet.length; i++) {
-		const letter = alphabet[i];
+	for (let i = 0; i < alphabetMapping.length; i++) {
+		const letter = alphabetMapping[i];
 
 		if (!letter.replacement) {
 			letter.replacement = "-";
 		}
 
-		// We don't use Regex `.replace()` because letters that *are* replaced
-		// which are later selected *for* replacement will be overwritten
+		// We don't use Regex `.replace()` because letters that *are replaced*
+		// which are later selected *for replacement* will be overwritten
 		for (let j = 0; j < outputAsStr.length; j++) {
 			const char = outputAsStr[j];
 
@@ -82,11 +56,6 @@ function crackCipher(error, output) {
 	}
 
 	console.log(decipheredStr);
-}
-
-function getOccurances(outputAsStr, letter) {
-	const match = new RegExp(`${ letter }`, "g");
-	return (outputAsStr.match(match) || []).length;
 }
 
 function replaceChar(decipheredStr, replacement, index) {
