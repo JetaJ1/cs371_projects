e0d22262f048d4f023f2f587d73baa6c05e0394f
Dakota Morgan <dakota_morgan@outlook.com>
2023-04-23 21:15:35 -0400

    Prune code

--- pa3_cs371/Cryptanalysis/freqAnalyze.py
+++ pa3_cs371/Cryptanalysis/freqAnalyze.py
@@ -24,47 +24,38 @@ else:
 # Get string from file input
 ####################################
 
-#test_str = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
-            # Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \
-            # Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \
-            # Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 # f = open("./PA3support/ciphertext.txt", "r")
 # f = open("./PA3support/hamlet.txt", "r")
 # f = open("./PA3support/merchantofvenice.txt", "r")
 
 f = open(file_path, "r", errors="ignore")               # Ignore errors, specifically in `merchantofvenice.txt`
-test_str = f.read() 
+in_str = f.read() 
 f.close()
 
 
-total_chars = len(test_str)
-test_str = test_str.lower()
-# print(test_str)
+total_chars = len(in_str)
+in_str = in_str.lower()
+# print(in_str)
 
 ####################################
 # Letter Frequency
 ####################################
 
 # Get frequency
-letters = map(''.join, zip(test_str))                   # Get all characters
+letters = map(''.join, zip(in_str))                     # Get all characters
 letters = list(filter(filterAlpha, letters))            # Filter characters for only letters
 letter_freq = Counter(letters)                          # Count occurances of letters
 
 # Sort char freqencies in descending order
 letter_freq = list(letter_freq.items())
 letter_freq.sort(reverse=True, key=(lambda x: x[1]))
-
-# print("\nLetter Frequency: ")
-# Print top 30 char frequencies
-# for pair in char_freq[0:30]:
-#     print(pair)
     
 ####################################
 # Bigram Frequency
 ####################################
 
 # Get frequency
-bigrams = map(''.join, zip(test_str, test_str[1:]))     # Get all bigrams
+bigrams = map(''.join, zip(in_str, in_str[1:]))         # Get all bigrams
 bigrams = list(filter(filterAlpha, bigrams))            # Filter bigrams for those containing only letters
 bigram_freq = Counter(bigrams)                          # Count occurances of bigrams
 
@@ -74,30 +65,20 @@ bigram_freq = Counter(bigrams)                          # Count occurances of bi
 bigram_freq_sorted = list(bigram_freq.items())
 bigram_freq_sorted.sort(reverse=True, key=(lambda x: x[1]))
 
-# print("\nBigram Frequency: ")
-# Print top 30 bigram frequencies
-# for pair in bigram_freq_sorted[0:30]:
-#     print(pair)
-
 ####################################
 # Trigram Frequency
 ####################################
 
 # Get bigrams frequency and store into dict
-trigrams = map(''.join, zip(test_str, test_str[1:], test_str[2:]))      # Get all trigrams
-trigrams = list(filter(filterAlpha, trigrams))                          # Filter trigrams for those containing only letters
-trigram_freq = Counter(trigrams)                                        # Count occurances of trigrams
+trigrams = map(''.join, zip(in_str, in_str[1:], in_str[2:]))    # Get all trigrams
+trigrams = list(filter(filterAlpha, trigrams))                  # Filter trigrams for those containing only letters
+trigram_freq = Counter(trigrams)                                # Count occurances of trigrams
 
 # print(trigram_freq)
 
 # Sort trigram freqencies in descending order
 trigram_freq_sorted = list(trigram_freq.items())
 trigram_freq_sorted.sort(reverse=True, key=(lambda x: x[1]))
-
-# print("\nTrigram Frequency: ")
-# Print top 30 trigram frequencies
-# for pair in trigram_freq_sorted[0:30]:
-#     print(pair)
 
 print()
 while len(letter_freq) < 30:            # For printing
